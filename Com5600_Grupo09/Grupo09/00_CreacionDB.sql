3e42280db3e5414eb1705a28f8ca3c0d3d6557d2
Daiana Ibarra <daianaibarra@outlook.es>
2025-10-29 22:12:41 -0300

    Subida inicial

---
+++ Com5600_Grupo09/Grupo09/00_CreacionDB.sql
@@ -0,0 +1,16 @@
+/**********************************************************
+ * Bases de Datos Aplicada - Comisión 5600
+ * GRUPO 09
+ *
+ * Archivo: 00_Creacion_BD
+ * Enunciado cumplimentado: Creación de las tablas del proyecto.
+ *
+ * Integrantes:
+ * - Ludmila Daiana Ibarra Sánchez DNI 42537300
+ * - Gianluca Ferreyra DNI 
+ * - Rodrigo Ezequiel Maranzana DNI 41853030
+ *********************************************************/
+
+CREATE DATABASE Com5600G09
+GO
+
